58e5badcc50db2268d3105b11579d38ff9cf7588
Joel Tinx <wizard.joel@gmail.com>
2017-04-14 22:57:46 -0500

    Add version info file

--- config.bat
+++ config.bat
@@ -1,3 +1,23 @@
-set GOPATH=C:\Users\JOEL\Desktop\Go\GoUI
-code .
-CLS
+## instalation steps
+
+mkdir GoUI && cd GoUI
+
+set GOPATH=./GoUI
+
+set PATH=%PATH%;C:\Users\JOEL\Desktop\Go\GoUI\bin
+
+go get github.com/JoelTinx/GoUI
+
+go get github.com/lxn/walk
+
+go get github.com/Knetic/govaluate
+
+go get github.com/akavel/rsrc
+
+cd src/github.com/JoelTinx/GoUI
+
+rsrc -manifest main.manifest -o rsrc.syso
+
+go build
+
+go build -ldflags="-H windowsgui"
